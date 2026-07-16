4e0f78a7290121f81cb2aeb7c6b6b4511cadcf2a
Jéssica Oliveira Carneiro <oliveiracarneirojessica@gmail.com>
2026-07-15 21:35:03 -0300

    Mudando imagem de fundo

--- jogo.py
+++ jogo.py
@@ -17,11 +17,18 @@ bombas = []
 ultimo_spaw = pygame.time.get_ticks()#calcular o último tempo em que o jogo começou a rodar
 intervalo = 1000# cria uma bomba a cada 1 segundo
 
-imagem_bombas = pygame.image.load("Piskel - Bomb.png")
+imagem_bombas = pygame.image.load("Piskel - Bomb.png").convert_alpha()
 imagem_nave = pygame.image.load("Piskel - Nave.png").convert_alpha()
 
 imagem_fundo = pygame.image.load("galaxia.jpg").convert_alpha()
 imagem_fundo_redimensionada = pygame.transform.scale(imagem_fundo,(altura,largura))
+
+
+#posição inicial das imagens de fundo
+imagem_1 = 0
+imagem_2 = -largura
+
+velocidade_imagem = 3
 
 tamanho_nave = 100
 imagem_nave_redimensionada = pygame.transform.scale(imagem_nave,(tamanho_nave,tamanho_nave))
@@ -43,8 +50,22 @@ fim_jogo = False
 
 while not fim_jogo:
 
-    tela.blit(imagem_fundo_redimensionada,(0,0))
+    #move as duas imagens para baixo
+    imagem_1 += velocidade_imagem
+    imagem_2 += velocidade_imagem
 
+    #se as imagens 1 e 2 sairem da tela, reposicione
+    if imagem_1 >= largura:
+        imagem_1 = -largura +  (imagem_1 - largura)
+
+    if imagem_2 >= largura :
+        imagem_2 = -largura + (imagem_2 - largura)
+
+    
+    tela.blit(imagem_fundo_redimensionada,(0,imagem_1))
+    tela.blit(imagem_fundo_redimensionada,(0,imagem_2))
+    
+    
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             fim_jogo = True
@@ -79,10 +100,10 @@ while not fim_jogo:
     if agora - ultimo_spaw >= intervalo:
         posicao_x = random.randint(0,750)#posições aleatórias entre 0 e 750 na horizontal
         posicao_y = -50#começar da posição -50 na vertical 
-        velocidade_bomba = random.randint(2,3)
+        velocidade_bomba = random.randint(4,5)
         tamanho_bomba = 100
         imagem_bomba_redimensionada  = pygame.transform.scale(imagem_bombas,(tamanho_bomba,tamanho_bomba))
-        bombas.append([posicao_x, posicao_y, velocidade_bomba, imagem_bomba_redimensionada])
+        bombas.append([posicao_x, posicao_y, velocidade_bomba])
 
         ultimo_spaw = agora#atualizar para o último tempo
 
@@ -91,7 +112,7 @@ while not fim_jogo:
         if bomba[1] > 600:#se a posisão da bomba na vertical for maior que o limite da tela...
             bombas.remove(bomba)#ela é apagada
         else:#senão
-            tela.blit(bomba[3],(bomba[0],bomba[1]))#será desenhada na tela
+            tela.blit(imagem_bomba_redimensionada,(bomba[0],bomba[1]))#será desenhada na tela
 
     pygame.display.flip()#atualização da tela para apróximos frames
     relogio.tick(60)# fps máximo suportado
